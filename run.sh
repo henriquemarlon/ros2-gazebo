54b4e6651dcd291e23ea179a0f2b407d4f7ed2d0
Henrique Marlon <henriquemarloncs@gmail.com>
2023-05-21 21:43:38 -0300

    Revert "feat(): first commit"
    This reverts commit 812e34647132fe06d50624f44ea9eb7de0933e84.

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-export TURTLEBOT3_MODEL=burger
-export LIBGL_ALWAYS_SOFTWARE=true
-ros2 launch turtlebot3_gazebo empty_world.launch.py
--- src/modules/__init__.py
+++
@@ -1,3 +0,0 @@
-from modules.stack import Stack
-from modules.controller import Controller
-from modules.queue import Queue
